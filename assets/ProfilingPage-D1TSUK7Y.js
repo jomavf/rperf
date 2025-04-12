a4cbb3f3cdd83a11cb20859927718e83eca69f0a
Jose Veliz <jose.veliz.francia@gmail.com>
2025-04-12 13:06:23 -0500

    Updates

--- assets/ProfilingPage-D1TSUK7Y.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-w5awoWBm.js";const i=()=>n.jsxs("div",{children:[n.jsx("h1",{children:"Component Profiling"}),n.jsx("p",{children:"Coming soon..."})]});export{i as default};
---
+++ assets/VirtualListsPage-YeCkQC7z.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index--ZQpdaMo.js";import r from"./index-DcCCpxT4.js";const a=()=>t.jsx(r,{});export{a as default};
