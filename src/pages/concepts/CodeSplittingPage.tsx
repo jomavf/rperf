5c132230fa5227e871ff5055a95be0a9351c9299
Jose Veliz <jose.veliz.francia@gmail.com>
2025-04-12 11:56:23 -0500

    Remove button for showing sales report in CodeSplittingPage and directly render SalesChart component, simplifying the user interface.

--- src/pages/concepts/CodeSplittingPage.tsx
+++ src/pages/concepts/CodeSplittingPage.tsx
@@ -261,14 +261,7 @@ function Dashboard() {
               </div>
             )}
 
-            <button
-              className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-              onClick={() => setShowBadExample(true)}
-            >
-              Show Sales Report
-            </button>
-
-            {showBadExample && <SalesChart />}
+            {<SalesChart />}
           </div>
         </CodeExample>
 
